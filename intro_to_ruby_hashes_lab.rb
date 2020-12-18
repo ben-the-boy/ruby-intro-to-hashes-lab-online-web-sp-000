3b72e9ab68107f26153f3dd84315a6e9354e8f9d
Ben Hayes <b.t.hayes@comcast.net>
2020-12-18 18:09:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,17 +7,18 @@ def actor
 end
 
 def monopoly
-	monopoly = {railroads: {
-	  pieces: 4,
-	  names: {},
-	  rent: {}
-	  }
-	}
+
 
 end
 
 def monopoly_with_second_tier
 	monopoly = {railroads: {}}
+		monopoly = {railroads: {
+	  pieces: 4,
+	  names: {},
+	  rent: {}
+	  }
+	}
 
 end
 
